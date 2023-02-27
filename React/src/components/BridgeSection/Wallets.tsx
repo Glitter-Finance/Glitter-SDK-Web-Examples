e404fae7b7a23197a6d4a245f15721bb7a751457
Ali Sajjad <66640805+alirizvi512@users.noreply.github.com>
2023-02-27 17:14:17 +0500

    Committing Solfare, Phantom and Metamask Integration

--- React/src/components/BridgeSection/Wallets.tsx
+++ React/src/components/BridgeSection/Wallets.tsx
@@ -1,8 +1,47 @@
-import {Box, Card, CardContent, CardHeader, Stack, Typography} from "@mui/material";
+import {Box, Card, CardContent, CardHeader, Link, Stack, Typography} from "@mui/material";
 import Item from "../Item";
 import React from "react";
+import {Metamask, Phantom, Solfare} from "glitter-bridge-sdk-web-dev";
 
 function Wallets() {
+
+  const onMetamask = () => {
+    const metamask = new Metamask();
+    metamask.connect()
+      .then((response: any) => {
+        console.log(response);
+        metamask.getProvider()
+          .then((provider) => {
+            console.log(provider);
+          })
+      })
+  }
+
+  const onPhantom = () => {
+    const phantom = new Phantom("https://api.mainnet-beta.solana.com");
+    phantom.getProvider().then((response) => {
+      console.log(response);
+      phantom.connect()
+        .then((response) => {
+          console.log("Response", response)
+        })
+        .catch((err) => {
+          console.log("Error", err)
+        })
+    })
+  }
+
+  const onSolfare = () => {
+    const solfare = new Solfare("https://api.mainnet-beta.solana.com");
+    solfare.connect()
+      .then((response) => {
+        console.log("Response", response)
+      })
+      .catch((err) => {
+        console.log("Error", err)
+      })
+  }
+
   return(
     <Card className="basis-card-layout">
       <CardHeader className="basic-card-header basic-card-header-typo" title="Connect Wallets"/>
@@ -12,15 +51,7 @@ function Wallets() {
             <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
               <img src="/phantom.png" className="input-image"/>
               <Typography>
-                {"4iVQ242Ck7azm9bu8HapFt7qZzkW6bKVXb3cXhK6vfEj".slice(0, 20)}...
-              </Typography>
-            </Box>
-          </Item>
-          <Item>
-            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <img src="/phantom.png" className="input-image"/>
-              <Typography>
-                {"4iVQ242Ck7azm9bu8HapFt7qZzkW6bKVXb3cXhK6vfEj".slice(0, 20)}...
+                <Link onClick={() => { onPhantom() }}>Phantom</Link>
               </Typography>
             </Box>
           </Item>
@@ -28,7 +59,15 @@ function Wallets() {
             <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
               <img src="/solflare.png" className="input-image"/>
               <Typography>
-                {"4iVQ242Ck7azm9bu8HapFt7qZzkW6bKVXb3cXhK6vfEj".slice(0, 20)}...
+                <Link onClick={() => { onSolfare() }}>Solfare</Link>
+              </Typography>
+            </Box>
+          </Item>
+          <Item>
+            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <img src="/phantom.png" className="input-image"/>
+              <Typography>
+                <Link onClick={() => { onMetamask() }}>Metamask</Link>
               </Typography>
             </Box>
           </Item>
